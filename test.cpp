37fce248a192db51aaa92c9cd61dc24c8c6b8b1d
faraz-pk <faraz.itpk@gmail.com>
2026-09-18 21:43:07 +0500

    Handle additional edge cases 2

--- test.cpp
+++ test.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 bool isSorted(const int *arr, const int size)
 {
+    if (size <= 1) return true;
     for (int i = 0; i < size - 1; i++)
     {
         if (arr[i] > arr[i + 1])
