6f97ea5e27b5276def806767ce88741cbf1aa5d0
faraz-pk <faraz.itpk@gmail.com>
2026-09-18 21:24:18 +0500

    Handle additional edge cases

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@ bool isSorted(const int *arr, const int size)
 {
     for (int i = 0; i < size - 1; i++)
     {
-        if (arr[i] < arr[i + 1])
+        if (arr[i] > arr[i + 1])
         {
             return false;
         }
